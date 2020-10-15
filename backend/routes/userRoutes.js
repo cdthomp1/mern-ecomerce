05baf904e92fd70e48c8096ef35281d71cde282c
cdthomp1 <tho17015@byui.edu>
2020-10-15 13:07:29 -0600

    Working on user auth

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+import { authUser } from '../controlers/userController.js'
+
+
+router.post('/login', authUser)
+
+
+export default router;
