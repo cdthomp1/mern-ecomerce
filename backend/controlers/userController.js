9c55ba6e4ba0941174e79ca67bff9ad32bd27513
cdthomp1 <tho17015@byui.edu>
2020-10-20 21:50:14 -0600

    user auth and registration

--- backend/controlers/userController.js
+++ backend/controlers/userController.js
@@ -1,21 +1,22 @@
 import AsyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js'
 
 // @desc    Auth user & get token
 // @route   Post /api/users/login
 // @access  Public
 const authUser = AsyncHandler(async (req, res) => {
-   const {email, password} = req.body
+    const { email, password } = req.body
 
-    const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
-    if(user && (await user.matchPassword(password))) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
@@ -24,6 +25,58 @@ const authUser = AsyncHandler(async (req, res) => {
 
 })
 
+// @desc    Register a new user
+// @route   Post /api/users
+// @access  Public
+const registerUser = AsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists!')
+    }
+
+    const user = await User.create({ name, email, password })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data');
+    }
+
+})
+
+// @desc    Get user profile
+// @route   Post /api/users/profile
+// @access  Private
+const getUserProfile = AsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found!")
+    }
+})
+
 export {
-    authUser
+    authUser,
+    registerUser,
+    getUserProfile
 }
